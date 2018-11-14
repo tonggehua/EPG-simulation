96197bcea27620e0550a0cabca7f9900a941a51d
Gehua Tong <31249056+tonggehua@users.noreply.github.com>
2018-11-14 14:51:21 -0500

    Delete gettingEPGfigures_data.m

--- gettingEPGfigures_data.m
+++
@@ -1,7 +0,0 @@
-%% (A) Validation with paper
-clear all
-close all
-%om_store_TSE = EPGdemo_TSE(120,3,20);
-om_store_GRE = EPGdemo_GRE(30,4,10,'default');
-%% (B) TSE simulation (1 us)
-%% (C) Echo intensity 
